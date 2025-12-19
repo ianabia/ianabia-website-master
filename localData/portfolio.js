194e1cee65aceb98980a93bc80a74394a642acde
Beatriz Nóbrega <beatriznobrega34@outlook.com>
2025-12-19 12:19:39 -0300

    add project dash netflix

--- localData/portfolio.js
+++ localData/portfolio.js
@@ -24,6 +24,24 @@ const portfolio = {
             ],
             live_url: ''
         },
+        {
+            id: 2,
+            image: '/images/portfolio/netflix_cover.png',
+            gallery: [
+                { image: '/images/portfolio/netflix/dashboard_netflix.png', title: 'Dashboard Netflix' },
+            ],
+            name: 'Interactive Dashboard – Netflix Account Analysis (2016–2025)',
+            client: 'Personal Project',
+            description: '<p>This interactive dashboard was developed using real data from my personal Netflix account, covering the period from 2016 to 2025. Based on data provided by the platform, the project delivers an in-depth analysis of viewing behavior, including total hours watched, volume of titles consumed, and playback distribution by device and user. The goal of this project is to transform raw data into clear insights through data visualization and exploratory analysis.</p>',
+            stacks: [
+                { logo: 'logos:microsoft-power-bi', name: 'Power BI' },
+                { logo: 'devicon:mysql', name: 'SQL' },
+            ],
+            tags: [
+                'Dashboard', 'Data Visualization', 'Exploratory Data Analysis'
+            ],
+            live_url: ''
+        },
     ]
     ,
     
@@ -48,6 +66,24 @@ const portfolio = {
             ],
             tags: [
                 'Game', 'Web application', 'UX/UI'
+            ],
+            live_url: ''
+        },
+        {
+            id: 2,
+            image: '/images/portfolio/netflix_cover.png',
+            gallery: [
+                { image: '/images/portfolio/netflix/dashboard_netflix.png', title: 'Dashboard Netflix' },
+            ],
+            name: 'Dashboard Interativo – Análise da Conta Netflix (2016–2025)',
+            client: 'Projeto Pessoal',
+            description: '<p>Este dashboard interativo foi desenvolvido a partir de dados reais da minha conta pessoal da Netflix, abrangendo o período de 2016 a 2025. Com base nas informações disponibilizadas pela plataforma, o projeto apresenta uma análise detalhada do comportamento de consumo, incluindo o total de horas assistidas, o volume de títulos consumidos e a distribuição das reproduções por dispositivo e por usuário. O objetivo é transformar dados brutos em insights claros por meio de visualização de dados e análise exploratória.</p>',
+            stacks: [
+                { logo: 'logos:microsoft-power-bi', name: 'Power BI' },
+                { logo: 'devicon:mysql', name: 'SQL' },
+            ],
+            tags: [
+                'Dashboard', 'Visualização de Dados', 'Análise Exploratória de Dados'
             ],
             live_url: ''
         },
